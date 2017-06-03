16d43f22d469ef57bbbfd2a9378d6de3eea675bb
Hanjun Dai <daihanjun@gmail.com>
2017-06-03 16:40:59 -0700

    sync

--- code/data_process/bamm_dream5.py
+++ code/data_process/bamm_dream5.py
@@ -21,11 +21,18 @@ def load_data():
             ll.append((val, st))
     return ll
 
-def output_file(idxes, suffix):
-    with open('%s/%s.fasta' % (out_root, suffix), 'w') as f:
-        for i in idxes:
-            f.write('>seq%d\n' % i)
-            f.write('%s\n' % dna_list[i])
+def output_file(fold, idxes, suffix):
+    ll = []
+    for i in idxes:
+        ll.append((i, dna_list[i]))
+    ll = sorted(ll, key = lambda x : x[1][0], reverse=True)
+
+    with open('%s/%s-%d.fasta' % (out_root, suffix, fold), 'w') as f:
+        with open('%s/%s-%d.label' % (out_root, suffix, fold), 'w') as f_label:
+            for i in range(len(ll)):
+                f.write('>seq%d\n' % ll[i][0])
+                f.write('%s\n' % ll[i][1][1])
+                f_label.write('%.10f\n' % ll[i][1][0])
 
 if __name__ == '__main__':
     dataset = sys.argv[1]
@@ -35,7 +42,7 @@ if __name__ == '__main__':
     if os.path.isdir(out_root):
         shutil.rmtree(out_root)
     os.makedirs(out_root)
-    dna_list = load_strs()
+    dna_list = load_data()
     train_idx = load_idx('%s/train.txt' % data_root)
     test_idx = load_idx('%s/test.txt' % data_root)
     assert len(train_idx)  + len(test_idx) == len(dna_list)
