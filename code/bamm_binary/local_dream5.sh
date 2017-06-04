b2a66f580505e1fcef56fcd763292cb601aa4b12
Hanjun Dai <daihanjun@gmail.com>
2017-06-04 10:31:29 -0700

    sync

--- code/bamm_binary/local_dream5.sh
+++ code/bamm_binary/local_dream5.sh
@@ -2,10 +2,10 @@
 
 data_root=../../bamm_data
 tool_root=$HOME/data/Playground/BaMMmotif/build
-frac=0.5
-dataset=ace2
-fold=1
-output_folder=$HOME/scratch/results/BaMM_12kmer/$dataset-$frac-fold-$fold
+frac=$1
+dataset=$2
+has_rev=$3
+output_folder=$HOME/scratch/results/BaMM_dream5/$dataset-$frac-inv-$has_rev
 
 if [ ! -e $output_folder ]; 
 then
@@ -14,16 +14,38 @@ fi
 
 python split_data.py \
     $frac \
-    $data_root/$dataset/train-${fold}.fasta \
+    $data_root/$dataset/train.fasta \
     $output_folder 
+
+if [ "$has_rev" == "1" ];
+then
 
 $tool_root/BaMMmotif \
 	$output_folder \
-	$output_folder/pos-train-${fold}.fasta \
+	$output_folder/pos-train.fasta \
 	-K 8 \
 	--reverseComp \
-	--negSequenceSet $output_folder/neg-train-${fold}.fasta \
-	--scoreTestSequenceSet $data_root/$dataset/test-${fold}.fasta \
+	--negSequenceSet $output_folder/neg-train.fasta \
+	--scoreTestSequenceSet $data_root/$dataset/test.fasta \
 	--scorePosSequenceSet \
 	--scoreNegSequenceSet
 	
+else
+
+$tool_root/BaMMmotif \
+	$output_folder \
+	$output_folder/pos-train.fasta \
+	-K 8 \
+	--negSequenceSet $output_folder/neg-train.fasta \
+	--scoreTestSequenceSet $data_root/$dataset/test.fasta \
+	--scorePosSequenceSet \
+	--scoreNegSequenceSet
+fi
+	
+python test_pred.py \
+	$output_folder
+
+python parse_result.py \
+	$data_root/$dataset/test.label \
+	$output_folder/test.pred \
+	2>&1 | tee $output_folder/result.txt
